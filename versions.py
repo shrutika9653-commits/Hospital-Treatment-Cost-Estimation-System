395372e220e743517ae32c87f6baa734d004cbef
shrutika9653-commits <shrutika9653@gmail.com>
2026-07-03 19:00:21 +0530

    Add files via upload

---
+++ versions.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import sklearn as sk
+import streamlit as st
+
+
+print(np.__version__)
+print(pd.__version__)
+print(sk.__version__)
+print(st.__version__)
